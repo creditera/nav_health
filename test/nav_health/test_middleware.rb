4f9ba6bc0850f1bbaa2510e72454c6f84c5285e1
James Thompson <jwthompson2@gmail.com>
2018-05-24 11:13:00 -0600

    Added Rails logging when an error occurs.

--- test/nav_health/test_middleware.rb
+++ test/nav_health/test_middleware.rb
@@ -1,5 +1,22 @@
 require 'test_helper'
 
+class MockLogger
+  attr_reader :messages
+  def initialize
+    @messages = []
+  end
+
+  def info(message)
+    @messages << message
+  end
+end
+
+module ::Rails
+  def self.logger
+    @logger ||= MockLogger.new
+  end
+end
+
 class TestMiddleware < MiniTest::Test
   def test_exists
     assert defined?(NavHealth::Middleware)
@@ -56,5 +73,20 @@ class TestMiddleware < MiniTest::Test
     result = middleware.call env
 
     assert_equal 500, result[0]
+  end
+
+  def test_logs_errors_to_rails_logger_when_present
+    env = { 'REQUEST_PATH' => '/nav_health' }
+    app = -> (env) { body }
+    NavHealth::Check.config do |check|
+      check.components.add 'db' do
+        false
+      end
+    end
+
+    middleware = NavHealth::Middleware.new app
+    result = middleware.call env
+
+    assert_includes ::Rails.logger.messages, result[2].first
   end
 end
