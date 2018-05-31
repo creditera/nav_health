f326cc9e52e8af2600cecf8119e5fcd28d352b75
James Thompson <jwthompson2@gmail.com>
2018-05-31 09:58:00 -0600

    Added explicit connection retrieval for ActiveRecord health check.

--- nav_health.gemspec
+++ nav_health.gemspec
@@ -29,4 +29,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "pry"
   spec.add_development_dependency "activerecord", "~> 5.0"
+  spec.add_development_dependency "sqlite3"
 end
--- test/nav_health/test_check.rb
+++ test/nav_health/test_check.rb
@@ -53,4 +53,49 @@ class TestCheck < MiniTest::Test
 
     assert_equal 'sonofa', result[:status]
   end
+
+  def test_run_with_activerecord
+    require "active_record"
+    NavHealth::Check.config do |middleware|
+      middleware.rails_app = true
+    end
+    ActiveRecord::Base.establish_connection(
+      adapter:  "sqlite3",
+      database: "tmp/test_db.sqlite3"
+    )
+
+    assert_equal false, ActiveRecord::Base.connected?
+    result = NavHealth::Check.run
+    assert_equal "allgood", result[:status]
+    ActiveRecord::Base.remove_connection
+  end
+
+  def test_run_with_activerecord_not_connected
+    require "active_record"
+    NavHealth::Check.config do |middleware|
+      middleware.rails_app = true
+    end
+
+    assert_equal false, !!ActiveRecord::Base.connected?
+    result = NavHealth::Check.run
+    assert_equal "sonofa", result[:status]
+    ActiveRecord::Base.remove_connection
+  end
+
+  def test_run_with_activerecord_connected
+    require "active_record"
+    NavHealth::Check.config do |middleware|
+      middleware.rails_app = true
+    end
+    ActiveRecord::Base.establish_connection(
+      adapter:  "sqlite3",
+      database: "tmp/test_db.sqlite3"
+    )
+    ActiveRecord::Base.retrieve_connection
+
+    assert_equal true, ActiveRecord::Base.connected?
+    result = NavHealth::Check.run
+    assert_equal "allgood", result[:status]
+    ActiveRecord::Base.remove_connection
+  end
 end
